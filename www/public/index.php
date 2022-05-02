0768de8a5d1cba768fb2a169140a5f4e7e33fc55
fernandodefez <fernandodefez@outlook.com>
2022-05-01 21:41:45 -0500

    database mysql8 dockerfile added and configured

--- www/public/index.php
+++ www/public/index.php
@@ -31,4 +31,36 @@ $router->set404(function () {
 });
 
 $router->get('/alex', function(){ echo "Ruta añadida por Alex"; });
+
+$router->get('/db', function() {
+    $link = mysqli_connect("database", "root", $_ENV['MYSQL_ROOT_PASSWORD'], null);
+
+    if (!$link) {
+        echo "Error: Unable to connect to MySQL." . PHP_EOL;
+        echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+        echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+        exit;
+    }
+
+    echo "Success: A proper connection to MySQL was made! The docker database is great." . PHP_EOL;
+
+    mysqli_close($link);
+});
+
+$router->get('/pdo', function() {
+    $DBuser = 'root';
+    $DBpass = $_ENV['MYSQL_ROOT_PASSWORD'];
+    $pdo = null;
+
+    try{
+        $database = 'mysql:host=database:3306';
+        $pdo = new PDO($database, $DBuser, $DBpass);
+        echo "Success: A proper connection to MySQL was made! The docker database is great.";
+    } catch(PDOException $e) {
+        echo "Error: Unable to connect to MySQL. Error:\n $e";
+    }
+
+    $pdo = null;
+});
+
 $router->run();
